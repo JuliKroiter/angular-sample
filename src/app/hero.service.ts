f2916443d4271c505be5ce819f47455d2359572f
JuliKroiter <kroiter09@gmail.com>
2018-01-13 17:33:50 +0200

    Search hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -29,6 +29,17 @@ export class HeroService {
       );
   }
 
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      // if not search term, return empty hero array.
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`api/heroes/?name=${term}`).pipe(
+      tap(_ => this.log(`found heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
   getHero(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`;
     return this.http.get<Hero>(url).pipe(
